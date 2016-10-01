9a7c585eb28fdf7698ddb62a7a082007577430c4
kjhark24 <kjhark24@gmail.com>
2016-10-01 15:16:46 -0400

    Lab 4 finished
    Its done!

---
+++ Lab4/MyApplication/app/src/main/java/com/example/kjh46/lab4/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.kjh46.lab4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/*
+ *@author Kyle Harkema
+ * Simple app that runs 2 activities on menu press
+ * For CS 262 Lab 4 Calvin College
+ * 9/30/16
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    /*
+     *Creates Option menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /*
+     * Checks when menu items are selected and preforms appropriate action
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //This is what starts activity 2
+            Intent myIntent = new Intent(this, Activity2.class);
+            startActivity(myIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
